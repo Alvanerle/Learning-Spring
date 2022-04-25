fc2692569ec35830970f6d99d1eaeb2fd1245866
Alvanerle <zzhumash03@gmail.com>
2022-04-25 21:04:33 +0600

    Lesson 7

---
+++ Lesson 7 Scopes/src/main/java/ru/imangali/spring/RapMusic.java
@@ -0,0 +1,8 @@
+package ru.imangali.spring;
+
+public class RapMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Rap song";
+    }
+}
---
+++ Lesson 7 Scopes/src/main/java/ru/imangali/spring/TestSpring.java
@@ -0,0 +1,31 @@
+package ru.imangali.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        firstMusicPlayer.playMusic();
+        secondMusicPlayer.playMusic();
+
+        System.out.println(firstMusicPlayer == secondMusicPlayer);
+
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(17);
+
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+
+
+        context.close();
+    }
+}
